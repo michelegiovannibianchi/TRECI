c4db936efb995a03200295db10049e83e5a6a2b7
michele.giovannibianchi <michele.bianchi@polito.it>
2025-10-10 18:05:14 +0200

    Upload main files

--- remove.sh
+++
@@ -1,8 +0,0 @@
-
-#!/bin/bash
-
-# Set the root directory (default to current directory if not provided)
-ROOT_DIR="."
-
-# Find and delete all .IDENTIFIER files
-find "$ROOT_DIR" -type f -name ".IDENTIFIER" -exec rm -v {} \;
